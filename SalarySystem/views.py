7df985446cc976e3777aa11e3bb1e9af056173d0
FantasY-bingo <1047389956@qq.com>
2020-04-26 00:09:25 +0800

    Initial commit

---
+++ SalarySystem/views.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Author ：Boyce Chen
+@Date   ：2020-04-25 23:39
+@Desc   ：
+=================================================="""
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+def login_view(request):
+    return HttpResponse('Hello World')
