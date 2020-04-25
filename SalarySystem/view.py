6d1f4b6bd3af3386286f9830ac9f844ea80af414
FantasY-bingo <1047389956@qq.com>
2020-04-25 23:51:52 +0800

    Initial commit

--- SalarySystem/view.py
+++ SalarySystem/view.py
@@ -5,7 +5,11 @@
 @Date   ：2020-04-25 23:39
 @Desc   ：
 =================================================="""
+from django.http import HttpResponseRedirect, HttpResponse
 
+
+def login_view(request):
+    return request.POST
 
 
 
