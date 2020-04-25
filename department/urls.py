9e317077ae40c0d68cd33e0c2b367be4913bcaf2
FantasY-bingo <1047389956@qq.com>
2020-04-25 23:52:00 +0800

    Initial commit

---
+++ department/urls.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Author ：Boyce Chen
+@Date   ：2020-04-25 23:50
+@Desc   ：部门子路由
+=================================================="""
+from django.conf.urls import url
+
+
+urlpatterns = [
+
+]
+
+
+
+
+
+
